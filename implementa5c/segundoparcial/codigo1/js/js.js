3fc6c3b01447d5544d2654de2c55a9bc56c3bee8
Army77hdz <alexxiaa2017@gmail.com>
2025-10-01 21:06:57 -0600

    Add files via upload

---
+++ implementa5c/segundoparcial/codigo1/js/js.js
@@ -0,0 +1,13 @@
+let n1=prompt("Nombre");
+let e1=parseFloat(prompt("Estatura 1"));
+
+let n2=prompt("Nombre");
+let e2=parseFloat(prompt("Estatura 2"));
+
+if(e1>e2){
+    console.log("El mas alto es:",n1);
+}
+else{
+    console.log("el mas alto es:",n2);
+}
+
