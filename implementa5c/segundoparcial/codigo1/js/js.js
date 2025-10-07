3c37607b641fc4adf15b1e81914e3ee209631a8d
Army77hdz <alexxiaa2017@gmail.com>
2025-10-07 11:50:52 -0600

    Add files via upload

--- implementa5c/segundoparcial/codigo1/js/js.js
+++ implementa5c/segundoparcial/codigo1/js/js.js
@@ -1,3 +1,10 @@
+   /*
+         Codigo:1
+    Objetivo: Conocer el ciclo simple en js
+    Fecha:29 de septiembre 2025
+    autor: By nandez REN
+    */
+
 let n1=prompt("Nombre");
 let e1=parseFloat(prompt("Estatura 1"));
 
